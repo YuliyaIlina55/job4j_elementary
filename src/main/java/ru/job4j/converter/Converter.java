4d5c99c9e6ec88bbaf3db5e19b540fe02d80a096
YuliyaIlina55 <flow987876765@gmail.com>
2022-02-22 02:06:07 +0900

    add test in file Converter.

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -30,5 +30,25 @@ public class Converter {
         System.out.println("12 euros are " + ruble + " ruble.");
         float ruble1 = Converter.dollarToRuble(6);
         System.out.println("6 dollars are " + ruble1 + " ruble.");
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result: " + passed);
+        float in1 = 360;
+        float expected1 = 6;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("360 rubles are 6. Test result: " + passed1);
+        float in2 = 12;
+        float expected2 = 840;
+        float out2 = Converter.euroToRuble(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("12 euros are 840. Test result: " + passed2);
+        float in3 = 6;
+        float expected3 = 360;
+        float out3 = Converter.dollarToRuble(in3);
+        boolean passed3 = expected3 == out3;
+        System.out.println("6 dollars are 360. Test result: " + passed3);
     }
 }
