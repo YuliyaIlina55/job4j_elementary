6d7573cc28c2e3a5196a7dc10e68a7eb42e16bfb
YuliyaIlina55 <flow987876765@gmail.com>
2022-02-22 18:26:54 +0900

    add ConverterTest for file Converter.

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert12EuroThen840Ruble() {
+        float in = 12;
+        float expected = 840;
+        float out = Converter.euroToRuble(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert360RblThen6Dollar() {
+        float in = 360;
+        float expected = 6;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert6dollarThen360Rbl() {
+        float in = 6;
+        float expected = 360;
+        float out = Converter.dollarToRuble(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
