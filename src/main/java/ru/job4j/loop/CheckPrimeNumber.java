eb5e47b26ddf561a185675fa1c3bad64fb44e3cb
YuliyaIlina55 <flow987876765@gmail.com>
2022-03-04 01:04:42 +0900

    Add file CheckPrimeNumber.

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int j = 2; j <= number; j++) {
+            if (number % number == 0 && number % 2 == 0 || number % number - 1 == 0) {
+                break;
+            }
+            return prime;
+        }
+        return false;
+    }
+}
