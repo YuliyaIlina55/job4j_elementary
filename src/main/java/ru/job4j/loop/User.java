005cf396f9e718e15a34269640d95fe81e06125f
YuliyaIlina55 <flow987876765@gmail.com>
2022-03-05 02:01:54 +0900

    Add file User.

---
+++ src/main/java/ru/job4j/loop/User.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class User {
+    public static String convert() {
+       String name = "Petr Arsentev";
+       return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(User.convert());
+    }
+}
+
