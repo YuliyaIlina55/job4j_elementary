e793d12c778a4fb9894c5e310bfac263cf153957
YuliyaIlina55 <flow987876765@gmail.com>
2022-03-05 23:49:02 +0900

    Corected file Counter.

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -7,16 +7,6 @@ public class Counter {
             sum = sum + i;
         }
         return sum;
-      }
-
-       public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
-        }
-        return sum;
-       }
+    }
 
 }
