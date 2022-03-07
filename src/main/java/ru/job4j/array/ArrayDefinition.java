f32ad8bbb59f41de3fba2798c1be8750724d5f93
Yuliya Ilina <flow987876765@gmail.com>
2022-03-07 15:59:30 +0900

    Added the array in file ArrayDefinition.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -13,8 +13,10 @@ public class ArrayDefinition {
         names[1] = "Eugene";
         names[2] = "Egor";
         names[3] = "Alena";
-        for (int i = 0; i < 4; i++) {
-            System.out.println(names[i]);
-        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
+
