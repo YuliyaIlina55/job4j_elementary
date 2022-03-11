0de35943792d46181fabd306c5c746f4a7b0017e
Yuliya Ilina <flow987876765@gmail.com>
2022-03-11 10:59:17 +0900

    Corrected EqLast

--- src/main/java/ru/job4j/array/EqLast.java
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -2,11 +2,9 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        for (int i = right.length - 1; i < left.length; i++) {
-            if (left[i] != right[i]) {
-                return false;
-            }
-        }
-        return true;
+
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
+
+
