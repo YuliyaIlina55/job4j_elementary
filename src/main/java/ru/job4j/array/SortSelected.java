b475b017f286f079d8a4f42d484ec50a99ac8119
Yuliya Ilina <flow987876765@gmail.com>
2022-03-14 14:15:02 +0900

    Corrected file SortSelected

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,13 +2,10 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int i = 0; i < data.length - 1; i++) {
-            int min = MinDiapason.findMin(data, i + 1, data.length - 1);
-            int index = FindLoop.indexOf2(data, min, i + 1, data.length - 1);
-            if (i != index) {
-                SwitchArray.swap(data, index - i, i);
-            }
-
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            SwitchArray.swap(data, index, i);
         }
         return data;
     }
