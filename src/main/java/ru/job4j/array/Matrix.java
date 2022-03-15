782af52a300d7444a18fb0f9f143f03ae089294d
Yuliya Ilina <flow987876765@gmail.com>
2022-03-15 18:17:29 +0900

    Corrected file Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class Matrix {
     public static int[][] multiple(int size) {
-        int[][] pifagor = new int[size][size];
-        for (int i = 0; i < pifagor.length; i++) {
-            for (int j = 0; j < pifagor[i].length; j++) {
-                pifagor[i][j] = (i + 1) * (j + 1);
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
             }
         }
-        return pifagor;
+        return matrix;
     }
 }
 
