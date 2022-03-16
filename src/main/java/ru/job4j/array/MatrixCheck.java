0aa5f6812b1d6f5308b412f50d04f300f0f0b7a1
Yuliya Ilina <flow987876765@gmail.com>
2022-03-16 09:38:18 +0900

    Corrected file MatrixCheck

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -28,7 +28,7 @@ public class MatrixCheck {
     }
 
     public static boolean isWin(char[][] board) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][i] == 'X') {
                 if (monoHorizontal(board, i) || monoVertical(board, i)) {
                     return true;
